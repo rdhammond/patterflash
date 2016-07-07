bda03857867f73208e41b8318d1b9031b067f469
Daniel Hammond <viztheman@gmail.com>
2016-07-07 14:34:03 -0400

    Added email service and confirmation email call.

--- ioc.js
+++ ioc.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const ChatRoomsService = require('./lib/services/ChatRoomsService'),
-  NicknamesService = require('./lib/services/NicknamesService'),
-  UsersService = require('./lib/services/UsersService');
+const ChatRoomsService = require('./lib/chat/ChatRoomsService'),
+  NicknamesService = require('./lib/chat/NicknamesService'),
+  UsersService = require('./lib/UsersService'),
+  EmailService = require('./lib/email/EmailService');
 
 let ioc = {
     express: require('express'),
@@ -13,20 +14,29 @@ let ioc = {
     crypto: require('crypto'),
     mongoose: require('mongoose'),
     FindOrCreate: require('mongoose-findorcreate'),
+    nodemailer: require('nodemailer'),
+    htmlToText: require('nodemailer-html-to-text').htmlToText,
+    uuid: require('uuid'),
 
     config: require('./config'),
     WebServer: require('./lib/web/WebServer'),
     ChatServer: require('./lib/chat/ChatServer'),
     ChatClient: require('./lib/chat/ChatClient'),
     UserRepository: require('./lib/db/UserRepository'),
-    Database: require('./lib/db/Database')
+    Database: require('./lib/db/Database'),
+
 };
 
 // Singletons
 //
+ioc.emailTemplates = {
+  confirmation: require('./lib/email/templates/confirmation')
+};
+
 ioc.UserModel = require('./lib/db/models/UserModel')(ioc);
 ioc.chatRoomsService = new ChatRoomsService();
 ioc.nicknamesService = new NicknamesService();
 ioc.usersService = new UsersService(ioc);
+ioc.emailService = new EmailService(ioc);
 
 module.exports = ioc;
